44fe89565077ea57b93b94997eb5ee820d2493bb
Kevin Earls <kearls@redhat.com>
2018-02-22 11:39:12 +0100

    Cleanup and simplify tracer creation; change default port to 6789

--- src/main/java/io/kearls/thriftexample/server/MyServer.java
+++ src/main/java/io/kearls/thriftexample/server/MyServer.java
@@ -24,30 +24,20 @@ import org.slf4j.LoggerFactory;
 import java.util.Map;
 
 public class MyServer {
-
     private static final Map<String, String> envs = System.getenv();
 
-    private static final Integer DELAY = new Integer(envs.getOrDefault("DELAY", "1"));
-    private static final Integer DURATION_IN_MINUTES = new Integer(envs.getOrDefault("DURATION_IN_MINUTES", "5"));
     private static final String JAEGER_AGENT_HOST = envs.getOrDefault("JAEGER_AGENT_HOST", "localhost");
-    private static final String JAEGER_COLLECTOR_HOST = envs.getOrDefault("JAEGER_COLLECTOR_HOST", "localhost");
-    private static final String JAEGER_COLLECTOR_PORT = envs.getOrDefault("MY_JAEGER_COLLECTOR_PORT", "14268");
     private static final Integer JAEGER_FLUSH_INTERVAL = new Integer(envs.getOrDefault("JAEGER_FLUSH_INTERVAL", "100"));
     private static final Integer JAEGER_MAX_PACKET_SIZE = new Integer(envs.getOrDefault("JAEGER_MAX_PACKET_SIZE", "0"));
     private static final Integer JAEGER_MAX_QUEUE_SIZE = new Integer(envs.getOrDefault("JAEGER_MAX_QUEUE_SIZE", "100000"));
     private static final Double JAEGER_SAMPLING_RATE = new Double(envs.getOrDefault("JAEGER_SAMPLING_RATE", "1.0"));
     private static final Integer JAEGER_UDP_PORT = new Integer(envs.getOrDefault("JAEGER_UDP_PORT", "6831"));
-    private static final String TEST_SERVICE_NAME = envs.getOrDefault("TEST_SERVICE_NAME", "standalone");
-    private static final Integer THREAD_COUNT = new Integer(envs.getOrDefault("THREAD_COUNT", "100"));
-    private static final String USE_AGENT_OR_COLLECTOR = envs.getOrDefault("USE_AGENT_OR_COLLECTOR", "COLLECTOR");
-    private static final String USE_LOGGING_REPORTER = envs.getOrDefault("USE_LOGGING_REPORTER", "false");
-
-    public static final String TRACES_CREATED_MESSAGE = "TRACES_CREATED: ";
+    private static final String SERVICE_NAME = envs.getOrDefault("SERVICE_NAME", "thriftTest    ");
 
     private static final Logger logger = LoggerFactory.getLogger(MyServer.class);
     private static Tracer tracer;
 
-    public static final int PORT = 9090;
+    public static final int PORT = 6789;
 
     /**
      *
@@ -57,34 +47,15 @@ public class MyServer {
      */
     private static Tracer jaegerTracer() {
         Tracer tracer;
-        Sender sender;
-        CompositeReporter compositeReporter;
 
-        if (USE_AGENT_OR_COLLECTOR.equalsIgnoreCase("agent")) {
-            sender = new UdpSender(JAEGER_AGENT_HOST, JAEGER_UDP_PORT, JAEGER_MAX_PACKET_SIZE);
-            logger.info("Using JAEGER tracer using agent on host [" + JAEGER_AGENT_HOST + "] port [" + JAEGER_UDP_PORT +
-                    "] Service Name [" + TEST_SERVICE_NAME + "] Sampling rate [" + JAEGER_SAMPLING_RATE
-                    + "] Max queue size: [" + JAEGER_MAX_QUEUE_SIZE + "]");
-        } else {
-            // use the collector
-            String httpEndpoint = "http://" + JAEGER_COLLECTOR_HOST + ":" + JAEGER_COLLECTOR_PORT + "/api/traces";
-            sender = new HttpSender(httpEndpoint);
-            logger.info("Using JAEGER tracer using collector on host [" + JAEGER_COLLECTOR_HOST + "] port [" + JAEGER_COLLECTOR_PORT +
-                    "] Service Name [" + TEST_SERVICE_NAME + "] Sampling rate [" + JAEGER_SAMPLING_RATE
-                    + "] Max queue size: [" + JAEGER_MAX_QUEUE_SIZE + "]");
-        }
-
+        Sender sender = new UdpSender(JAEGER_AGENT_HOST, JAEGER_UDP_PORT, JAEGER_MAX_PACKET_SIZE);
         Metrics metrics = new Metrics(new StatsFactoryImpl(new NullStatsReporter()));
         Reporter remoteReporter = new RemoteReporter(sender, JAEGER_FLUSH_INTERVAL, JAEGER_MAX_QUEUE_SIZE, metrics);
-        if (USE_LOGGING_REPORTER.equalsIgnoreCase("true")) {
-            Reporter loggingRepoter = new LoggingReporter(logger);
-            compositeReporter = new CompositeReporter(remoteReporter, loggingRepoter);
-        } else {
-            compositeReporter = new CompositeReporter(remoteReporter);
-        }
-
+        Reporter loggingRepoter = new LoggingReporter(logger);
+        CompositeReporter compositeReporter = new CompositeReporter(remoteReporter, loggingRepoter);
         Sampler sampler = new ProbabilisticSampler(JAEGER_SAMPLING_RATE);
-        tracer = new com.uber.jaeger.Tracer.Builder(TEST_SERVICE_NAME, compositeReporter, sampler)
+
+        tracer = new com.uber.jaeger.Tracer.Builder(SERVICE_NAME, compositeReporter, sampler)
                 .build();
 
         return tracer;
